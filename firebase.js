5cbda3884e6e84abb13698102cdbc57d4b9b902e
Dragon489 <34201972+Dragon489@users.noreply.github.com>
2017-12-07 22:41:01 -0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.8.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCFQ8jLSM-alcYlOMtbFdt645RLvnfEF0E",
+    authDomain: "jpl-101.firebaseapp.com",
+    databaseURL: "https://jpl-101.firebaseio.com",
+    projectId: "jpl-101",
+    storageBucket: "jpl-101.appspot.com",
+    messagingSenderId: "447755254569"
+  };
+  firebase.initializeApp(config);
+</script>
